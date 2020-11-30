c49345fc491948525ce6456a98d7accd0e611e6b
Philipp Schunker <philipp@schunker.at>
2020-11-30 20:54:24 +0100

    added IntVector2, MazeDirection, MazePassage, MazeWall

--- ue6-3dgame/Assets/Scripts/Maze.cs
+++ ue6-3dgame/Assets/Scripts/Maze.cs
@@ -4,24 +4,42 @@ using UnityEngine;
 
 public class Maze : MonoBehaviour {
 
-    public int sizeX, sizeZ;
+    //public int sizeX, sizeZ;
+    public IntVector2 size;
     public MazeCell cellPrefab;
+    public MazePassage passagePrefab;
+    public MazeWall wallPrefab;
     public float generationStepDelay;
     private MazeCell[,] cells;
+
+    public IntVector2 RandomCoordinates {
+        get {
+            return new IntVector2(Random.Range(0, this.size.x), Random.Range(0, this.size.z));
+        }
+    }
+
+    public bool ContainsCoordinates (IntVector2 coordinate) {
+        return coordinate.x >= 0 && coordinate.x < this.size.x && coordinate.z >= 0 && coordinate.z < this.size.z;
+    }
+
     // Start is called before the first frame update
     void Start() {
-        
+        //
     }
 
     // Update is called once per frame
     void Update() {
-        
+        //
+    }
+
+    public MazeCell GetCell(IntVector2 coordinates) {
+        return this.cells[coordinates.x, coordinates.z];
     }
 
     public void Generate() {
-        this.cells = new MazeCell[this.sizeX, this.sizeZ];
-        for (int x = 0; x < this.sizeX; x++) {
-            for (int z = 0; z < this.sizeZ; z++) {
+        this.cells = new MazeCell[this.size.x, this.size.z];
+        for (int x = 0; x < this.size.x; x++) {
+            for (int z = 0; z < this.size.z; z++) {
                 this.CreateCell(x, z);
             }
         }
@@ -29,20 +47,102 @@ public class Maze : MonoBehaviour {
 
     public IEnumerator GenerateWithDelay() {
         WaitForSeconds delay = new WaitForSeconds(this.generationStepDelay);
-        this.cells = new MazeCell[this.sizeX, this.sizeZ];
-        for (int x = 0; x < this.sizeX; x++) {
-            for (int z = 0; z < this.sizeZ; z++) {
+        this.cells = new MazeCell[this.size.x, this.size.z];
+        /*
+        for (int x = 0; x < this.size.x; x++) {
+            for (int z = 0; z < this.size.z; z++) {
                 yield return delay;
                 this.CreateCell(x, z);
             }
+        } */
+
+        List<MazeCell> activeCells = new List<MazeCell>();
+        this.DoFirstGenerationStep(activeCells);
+        while (activeCells.Count > 0) {
+            yield return delay;
+            this.DoNextGenerationStep(activeCells);
+        }
+        /*
+        IntVector2 coordinates = this.RandomCoordinates;
+
+        while (this.ContainsCoordinates(coordinates) && this.GetCell(coordinates) == null) {
+            yield return delay;
+            this.CreateCell(coordinates);
+            coordinates += MazeDirections.RandomValue.ConvertToIntVector2();
+        }
+        */
+    }
+
+    private void DoFirstGenerationStep(List<MazeCell> activeCells) {
+        MazeCell randomCell = this.CreateCell(RandomCoordinates);
+        activeCells.Add(randomCell);
+    }
+
+    private void DoNextGenerationStep(List<MazeCell> activeCells) {
+        int index = activeCells.Count - 1;
+        MazeCell cell = activeCells[index];
+
+        if (cell.IsFullyInitialized) {
+            activeCells.RemoveAt(index);
+            return;
+        }
+
+        //MazeDirection direction = MazeDirections.RandomValue;
+        MazeDirection direction = cell.RandomUnitializedDirection;
+        IntVector2 coordinates = cell.coordinates + direction.ConvertToIntVector2();
+        
+        if (this.ContainsCoordinates(coordinates) /*&& this.GetCell(coordinates) == null*/) {
+            //activeCells.Add(this.CreateCell(coordinates));
+            MazeCell neighbor = this.GetCell(coordinates);
+            if (neighbor == null) {
+                neighbor = this.CreateCell(coordinates);
+                this.CreatePassage(cell, neighbor, direction);
+                activeCells.Add(neighbor);
+                
+            } else {
+                this.CreateWall(cell, neighbor, direction);
+                //activeCells.RemoveAt(index);  //remove not needed
+            }
+            
+        } else {
+            this.CreateWall(cell, null, direction);
+            //activeCells.RemoveAt(index);  //remote not needed
         }
     }
 
-    private void CreateCell(int x, int z) {
+    private MazeCell CreateCell(int x, int z) {
         MazeCell cell = Instantiate(cellPrefab) as MazeCell;
         this.cells[x, z] = cell;
         cell.name = "Maze Cell " + x + ", " + z;
         cell.transform.parent = transform;
-        cell.transform.localPosition = new Vector3(x - this.sizeX * 0.5f + 0.5f, 0f, z - this.sizeZ * 0.5f + 0.5f);
+        cell.transform.localPosition = new Vector3(x - this.size.x * 0.5f + 0.5f, 0f, z - this.size.z * 0.5f + 0.5f);
+        return cell;
+    }
+
+    private MazeCell CreateCell(IntVector2 coordinates) {
+        MazeCell cell = Instantiate(cellPrefab) as MazeCell;
+        this.cells[coordinates.x, coordinates.z] = cell;
+        cell.coordinates = coordinates;
+        cell.name = "Maze Cell " + coordinates.x + ", " + coordinates.z;
+        cell.transform.parent = transform;
+        cell.transform.localPosition = new Vector3(coordinates.x - this.size.x * 0.5f + 0.5f, 0f, coordinates.z - this.size.z * 0.5f + 0.5f);
+        return cell;
+    }
+
+    private void CreatePassage(MazeCell cell, MazeCell otherCell, MazeDirection direction) {
+        MazePassage passage = Instantiate(passagePrefab) as MazePassage;
+        passage.Initialize(cell, otherCell, direction);
+        passage = Instantiate(passagePrefab) as MazePassage;
+        passage.Initialize(otherCell, cell, direction.GetOpposite());
+    }
+
+    private void CreateWall(MazeCell cell, MazeCell otherCell, MazeDirection direction) {
+        MazeWall wall = Instantiate(wallPrefab) as MazeWall;
+        wall.Initialize(cell, otherCell, direction);
+        
+        if (otherCell != null) {
+            wall = Instantiate(wallPrefab) as MazeWall;
+            wall.Initialize(otherCell, cell, direction.GetOpposite());
+        }
     }
 }
--- ue6-3dgame/Assets/Scripts/MazeCell.cs
+++ ue6-3dgame/Assets/Scripts/MazeCell.cs
@@ -3,13 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MazeCell : MonoBehaviour {
+
+    public IntVector2 coordinates;
+    private MazeCellEdge[] edges = new MazeCellEdge[MazeDirections.Count];
+    private int initializedEdgeCount;
+
+    public bool IsFullyInitialized {
+        get {
+            return initializedEdgeCount == MazeDirections.Count;
+        }
+    }
+
+    public MazeDirection RandomUnitializedDirection {
+        get {
+            int skips = Random.Range(0, MazeDirections.Count - this.initializedEdgeCount);
+            for (int i = 0; i < MazeDirections.Count; i++) {
+                if (this.edges[i] == null) {
+                    if (skips == 0) {
+                        return (MazeDirection)i;
+                    }
+                    skips--;
+                }
+            }
+            throw new System.InvalidOperationException("MazeCell has no unitialized directions left");
+        }
+    }
+
+    public MazeCellEdge GetEdge (MazeDirection direction) {
+        return this.edges[(int)direction];
+    }
+
+    public void SetEdge(MazeDirection direction, MazeCellEdge edge) {
+        this.edges[(int)direction] = edge;
+        this.initializedEdgeCount++;
+    }
+
     // Start is called before the first frame update
     void Start() {
-        
+        //
     }
 
     // Update is called once per frame
     void Update() {
-        
+        //
     }
 }
