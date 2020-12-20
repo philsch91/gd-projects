dd8ccac0c176ad46244f56761c3b723540516592
Philipp Schunker <philipp@schunker.at>
2020-12-20 22:26:11 +0100

    added ue6-3dgame2

---
+++ ue6-3dgame2/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public GameObject obstaclePrefab;
+    public int numberOfObstacles = 1000;
+    public float groundWidth = 15f;
+    public float minZ = 30.0f;
+    public float maxZ = 60.0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        //Vector3 position = new Vector3();
+        Vector3 position = obstaclePrefab.transform.position;
+        //Vector3 objectSize = obstaclePrefab.GetComponent<Renderer>().bounds.size;
+        float maxX = this.groundWidth / 2;
+        for (int i = 0; i < this.numberOfObstacles; i++) {
+            GameObject obstacle = obstaclePrefab;
+            Vector3 obstacleSize = obstacle.GetComponent<Renderer>().bounds.size;
+
+            position.x = Random.Range(-maxX, maxX);
+            position.y = 1f;
+            position.z += Random.Range(obstacleSize.z * 3, this.maxZ);
+
+            GameObject newObstacle = Instantiate(obstacle, position, Quaternion.identity);
+            Vector3 obstacleScale = newObstacle.transform.localScale;
+            obstacleScale.x += Random.Range(-0.5f, 0.5f);
+            newObstacle.transform.localScale = obstacleScale;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //
+    }
+}
---
+++ ue6-3dgame2/Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+    #pragma warning disable CS0108 // Element blendet vererbte Element aus; fehlendes 'new'-Schlüsselwort
+    private Rigidbody rigidbody;
+    #pragma warning restore CS0108 // Element blendet vererbte Element aus; fehlendes 'new'-Schlüsselwort
+    
+    // Start is called before the first frame update
+    void Start() {
+        this.rigidbody = GetComponent<Rigidbody>();
+        //this.rigidbody.AddForce(0, 200, 500);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //
+    }
+
+    private void FixedUpdate() {
+        this.rigidbody.AddForce(0, 0, this.forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d")) {
+            this.rigidbody.AddForce(this.sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a")) {
+            this.rigidbody.AddForce(-this.sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
